1bc1d7ca3b3ae79e97b263405d3daba610fc47a5
Jason He <jhe@magmacapitalfunds.com>
2023-02-10 01:25:48 -0600

    add test case for symbolic transform

--- run_cusr.cu
+++ run_cusr.cu
@@ -29,6 +29,8 @@ int main() {
     reg.max_program_depth = 10;    // better less than 20 --
                                    // or may cause overflow due to the limitation of the length of prefix (less than 2048)
     reg.population_size = 50;
+    reg.n_components = 5;
+    reg.n_hall_of_fame = 10;
     reg.generations = 50;
     reg.parsimony_coefficient = 0;            // this param prevents program from bloating  -- derived from "gplearn"
     reg.const_range = {-5, 5};      // the range of the constant of each program
@@ -36,7 +38,7 @@ int main() {
     reg.init_method = init_t::half_and_half;   // ramped half-and-half is recommended
     reg.tournament_size = 3;                   // it only supports tournament selection
     reg.metric = metric_t::root_mean_square_error; // also support MAE error and MSE error
-    reg.fit(dataset, real_value); // do training
+    reg.fit(dataset, real_value, "pearson"); // do training
 
     // after training
     cout << "execution time: " << reg.regress_time_in_sec << endl;
@@ -44,5 +46,9 @@ int main() {
     // optimal program
     cout << "best program (in prefix):  " << cusr::program::prefix_to_string(reg.best_program.prefix) << endl;
     cout << "best program (in infix) :  " << cusr::program::prefix_to_infix(reg.best_program.prefix) << endl;
+    // components
+    for(auto &prog : reg.components) {
+        cout << "component (in infix) :  " << cusr::program::prefix_to_infix(prog.prefix) << endl;
+    }
     return 0;
 }
